cd70a68785f64086772f5a5afe77c0c1fe2f5528
camilaliaudat <camilaliaudat@gmail.com>
2022-09-27 16:27:21 -0300

    agregamos mas testeos y uno que falla

--- unit-test/example.cpp
+++ unit-test/example.cpp
@@ -3,14 +3,29 @@
 using namespace testing;
 namespace foobar::test
 {
-    TEST(EsPar1, EsPar) //pide un nombre no estoy segura de si es eso
+    TEST(EsPar, EsPar) 
     {
-        //assert(EsPar(2), true);
         ASSERT_THAT(EsPar(20), true);
+    }
+    TEST(EsPar1, EsPar)
+    {
+        ASSERT_THAT(EsPar(17), false);
+    }
+    TEST(EsPar2, EsPar)
+    {
+        ASSERT_THAT(EsPar(1), false);
     }
     TEST(EsMax, Maximo)
     {
         ASSERT_THAT(Maximo(10, 15), 15);
+    }
+    TEST(EsMax1, Maximo)
+    {
+        ASSERT_THAT(Maximo(22, 1), 22);
+    }
+    TEST(EsMax2, Maximo)
+    {
+        ASSERT_THAT(Maximo(75, 15), 15);// CASO QUE LARGA ERROR
     }
 
 }
